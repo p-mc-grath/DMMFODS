69cc3b9717dc7e77a1cca5a7ffe3eb8ba8f29faa
pmcgrath249 <pmcgrath249@gmail.com>
2020-01-27 21:01:41 +0100

    init

--- WaymoData.py
+++ WaymoData.py
@@ -1,80 +1,68 @@
+import torch
+from os import listdir
+from os.path import join
 from torch.utils.data import Dataset, DataLoader
-import tensorflow as tf
-from waymo_open_dataset.utils import range_image_utils
-from waymo_open_dataset.utils import transform_utils
-from waymo_open_dataset.utils import  frame_utils
-from waymo_open_dataset import dataset_pb2 as open_dataset
+from Dense_U_Net_lidar_helper import load_dict
 
-class WaymoDataset:
-    def __init__(self, mode, data_root):
-        self.mode = mode
-        self.data_root = data_root
+class WaymoDataset(Dataset):
+    def __init__(self, rgb_root, lidar_root, label_root):
+
+        super().__init__()
+
+        # filenames incl path
+        self.rgb_files = [join(rgb_root, file) for file in listdir(rgb_root)]
+        self.lidar_files = [join(lidar_root, file) for file in listdir(lidar_root)]
+        self.label_files = [join(label_root, file) for file in listdir(label_root)]  
         
-        dataset = tf.data.TFRecordDataset(data_root, compression_type='')
-        for data in dataset:
-            frame = open_dataset.Frame()
-            frame.ParseFromString(bytearray(data.numpy()))
-
-            frame.camera_labels
-            for camera_labels in frame.camera_labels:
-                # Iterate over the individual labels.
-                for label in camera_labels.labels:
-                # Draw the object bounding box.
-                    label.box.center_x 
-                    label.box.center_y 
-                    label.box.width
-                    label.box.length,
-                    label.box.width,
-
-        ###
-            for index, image in enumerate(frame.images):
-                tf.image.decode_jpeg(image.image)
-
-            self.imgs
-            self.labels
+        self._check_data_integrity()
 
     def __getitem__(self, idx):
-        dataset.as_numpy_iterator()
-        dataset.range(index-1, index)
-        sample = dataset.from_tensor_slices[idx]
-        return sample
+
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        # load data corresponding to idx
+        image = torch.load(self.rgb_files[idx])    
+        lidar = torch.load(self.lidar_files[idx])
+        labels= load_dict(self.label_files[idx])
     
+        return {'image': image, 'lidar': lidar, 'labels': labels}
+
     def __len__(self):
-        return len(self.imgs)
+        return len(self.rgb_files)
+
+    def _check_data_integrity(self):
+        for i in range(len(self.rgb_files)):
+            assert self.lidar_files[i].endswith(self.rgb_files[i]), 'something in lidar data dir that does not belong' 
+            assert self.label_files[i].endswith(self.rgb_files[i]), 'something in label data dir that does not belong'
 
 class WaymoDataset_Loader:
-    def __init__(self, config):
-        '''
-        Args:
-        config.data_root
-        config.mode
-        config.batch_size
-        config.data_loader_workers
-        config.pin_memory
-        '''
-        self.config = config
-        assert self.config.mode in ['train', 'test']
+    # TODO batch size
+    # TODO num_workers
+    def __init__(self, mode, data_root, bn):
+        self.mode = mode
 
-        if self.config.mode == 'train':
-            train_set = WaymoDataset('train', self.config.data_root)
-            valid_set = WaymoDataset('val', self.config.data_root)
+        if self.mode == 'train':
+            train_set = WaymoDataset(join(data_root, 'train', 'images'), 
+                                    join(data_root, 'train', 'lidar'), 
+                                    join(data_root, 'train', 'labels'))
+            valid_set = WaymoDataset(join(data_root, 'val', 'images'), 
+                                    join(data_root, 'val', 'lidar'), 
+                                    join(data_root, 'val', 'labels'))
 
-            self.train_loader = DataLoader(train_set, batch_size=self.config.batch_size, shuffle=True,
-                                           num_workers=self.config.data_loader_workers,
-                                           pin_memory=self.config.pin_memory)
-            self.valid_loader = DataLoader(valid_set, batch_size=self.config.batch_size, shuffle=False,
-                                           num_workers=self.config.data_loader_workers,
-                                           pin_memory=self.config.pin_memory)
-            self.train_iterations = (len(train_set) + self.config.batch_size) // self.config.batch_size
-            self.valid_iterations = (len(valid_set) + self.config.batch_size) // self.config.batch_size
+            self.train_loader = DataLoader(train_set, batch_size=bn)
+            self.valid_loader = DataLoader(valid_set, batch_size=bn)
+            self.train_iterations = (len(train_set) + bn) // bn
+            self.valid_iterations = (len(valid_set) + bn) // bn
 
-        elif self.config.mode == 'test':
-            test_set = WaymoDataset('test', self.config.data_root)
+        elif self.mode == 'test':
 
-            self.test_loader = DataLoader(test_set, batch_size=self.config.batch_size, shuffle=False,
-                                          num_workers=self.config.data_loader_workers,
-                                          pin_memory=self.config.pin_memory)
-            self.test_iterations = (len(test_set) + self.config.batch_size) // self.config.batch_size
+            test_set = WaymoDataset(join(data_root, 'test', 'images'), 
+                                    join(data_root, 'test', 'lidar'), 
+                                    join(data_root, 'test', 'labels'))
+
+            self.test_loader = DataLoader(test_set, batch_size=bn)
+            self.test_iterations = (len(test_set) + bn) // bn
 
         else:
             raise Exception('Please choose a proper mode for data loading')
