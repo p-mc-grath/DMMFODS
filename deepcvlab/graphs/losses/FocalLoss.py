b55dddf545d5035b6c0663f52ab83bf9355de6b2
pmcgrath249 <pmcgrath249@gmail.com>
2020-06-11 18:02:59 +0200

    implemented class-wise focal loss

--- deepcvlab/graphs/losses/FocalLoss.py
+++ deepcvlab/graphs/losses/FocalLoss.py
@@ -1,5 +1,5 @@
 # Copied from https://www.kaggle.com/c/tgs-salt-identification-challenge/discussion/65938
-# Full credit to Allen Qin
+# Full credit to Allen Qin for the basic implementation of the original Focal Loss
 # paper: https://arxiv.org/abs/1708.02002
 
 import torch
@@ -34,13 +34,52 @@ class FocalLoss(nn.Module):
             loss
         '''
         if self.logits:
-            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
+            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
         else:
-            BCE_loss = F.binary_cross_entropy(inputs, targets, reduce=False)
-        pt = torch.exp(-BCE_loss)
+            BCE_loss = F.binary_cross_entropy(inputs, targets, reduction='none')
+        pt = torch.exp(-BCE_loss)                                                                       # -BCE_loss = log(pt)
         F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
 
         if self.reduce:
             return torch.mean(F_loss)
         else:
             return F_loss
+
+class ClassWiseFocalLoss(FocalLoss):
+    '''
+    See FocalLoss
+    + Allows to apply different alpha and gamma for each class
+        alpha: class-class imbalance
+        gamma: class-background imbalance
+    '''
+    def __init__(self, alpha=[1], gamma=[8, 13, 16], logits=True, reduce=False):
+        '''
+        See FocalLoss
+        Expects gamma and alpha to be of same length
+        '''
+        super().__init__(alpha, gamma, logits, reduce)
+
+    def forward(self, inputs, targets): 
+        '''
+        Expects targets and inputs to be structured like: batches x classes x X x Y
+        
+        Arguments:
+            see FocalLoss
+        return:
+            loss
+        '''
+        if self.logits:
+            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
+        else:
+            BCE_loss = F.binary_cross_entropy(inputs, targets, reduction='none')
+        pt = torch.exp(-BCE_loss)                                                                       # -BCE_loss = log(pt)
+        
+        F_loss = torch.zeros_like(pt)
+        for i, (alpha, gamma) in enumerate(zip(self.alpha, self.gamma)):
+            F_loss[:, i, :, :] = alpha * (1-pt[:, i, :, :])**gamma * BCE_loss[:, i, :, :]
+
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
+
